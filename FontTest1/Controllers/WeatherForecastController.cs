eb86ca86557ac36cdad7a8dc43a9c2e4727736f0
Chandima Ranaweera <chandima@bistecglobal.com>
2024-04-10 10:04:55 +0530

    PDF created

--- FontTest1/Controllers/WeatherForecastController.cs
+++ FontTest1/Controllers/WeatherForecastController.cs
@@ -2,8 +2,13 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using Spire.Doc;
+using Document = Spire.Doc.Document;
+using Spire.Doc.Documents;
+using Section = Spire.Doc.Section;
+using Spire.Doc.Fields;
 
 namespace FontTest1.Controllers
 {
@@ -34,6 +39,80 @@ namespace FontTest1.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("pdf")]
+        public Stream GeneratePDF()
+        {
+            var stream = System.IO.File.Open("CertificateTemplates/Docs/template.docx", FileMode.Open);
+            return GenerateCertificatePdf(stream, new Dictionary<string, object> { ["{{Title}}"] = "まはしく" });
+        }
+
+        private Stream GenerateCertificatePdf(Stream template, IDictionary<string, object> fieldValuePairs)
+        {
+            using (var document = new Document(template))
+            {
+                bool isDisplayAlternate = fieldValuePairs.Where(kvp => kvp.Key.Contains("isDisplayAlternate")).Any(key => (string)key.Value == "1");
+                if (!isDisplayAlternate)
+                {
+                    TextSelection selection = document.FindString("AlternateName", true, true);
+                    if (selection != null)
+                    {
+                        Paragraph paragraph = selection.GetAsOneRange().OwnerParagraph;
+                        Section section = paragraph.OwnerTextBody.Owner as Section;
+                        section.Paragraphs.Remove(paragraph);
+                    }
+                }
+                foreach (var keyValuePair in fieldValuePairs)
+                {
+                    switch (keyValuePair.Value)
+                    {
+                        case string newValue:
+                            if (keyValuePair.Key.Contains("AlternateName"))
+                            {
+                                TextSelection selection = document.FindString("AlternateName", false, true);
+                                if (selection != null)
+                                {
+                                    TextRange r = selection.GetAsOneRange();
+                                    Paragraph paragraph = r.OwnerParagraph;
+
+                                    paragraph.ChildObjects.Clear();
+
+                                    TextRange newTextRange = new TextRange(document);
+                                    newTextRange.Text = newValue;
+                                    document.EmbedFontsInFile = true;
+                                    string fontPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CertificateTemplates/Fonts/ARIALUNI.TTF");
+                                    if (System.IO.File.Exists(fontPath))
+                                    {
+                                        document.PrivateFontList.Add(new PrivateFontPath("Arial Unicode MS", fontPath));
+                                        newTextRange.CharacterFormat.FontName = "Arial Unicode MS";
+                                    }
+                                    paragraph.ChildObjects.Add(newTextRange);
+
+
+
+                                }
+                            }
+                            else
+                            {
+                                document.Replace(keyValuePair.Key, newValue, caseSensitive: false, wholeWord: true);
+                            }
+                            break;
+
+                        default:
+                            throw new NotImplementedException($"{keyValuePair.Value.GetType().FullName} = {keyValuePair.Value}");
+                    }
+                }
+
+                var output = new MemoryStream();
+
+                document.JPEGQuality = 100;
+                document.SaveToStream(output, Spire.Doc.FileFormat.PDF);
+
+                output.Seek(0, SeekOrigin.Begin);
+
+                return output;
+            }
         }
     }
 }
